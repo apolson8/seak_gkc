bcfab83abb851aff57bdcdf8c366347c12bf011f
kjpalof <katie.palof@alaska.gov>
2019-10-28 08:59:54 -0800

    move libraries and theme into helper file

--- r/harv_cpue.R
+++ r/harv_cpue.R
@@ -6,24 +6,9 @@
 # SEAK GKC Harvest and CPUE trends -----------
 
 # load ---------
-library(tidyverse)
-library(lubridate)
-library(readxl)
-library(ggthemes)
-library(gridExtra)
-library(extrafont)
-library(tidyr)
-library(padr)
-library(anytime)
+source("./r/helper.R")
 
-##THEMES FOR GRAPHS#####
-loadfonts(device="win")
-windowsFonts(Times=windowsFont("TT Times New Roman"))
-theme_set(theme_bw(base_size=14,base_family='Times New Roman')
-          +theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()))
 
-#COLOR BLIND PALETTE####
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 ## data -------------------------
 # Import fishticket and logbook data from ALEX
